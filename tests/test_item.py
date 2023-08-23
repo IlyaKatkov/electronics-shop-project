a5932c53b6b308bcce3b031d0cf2109a4a0003a0
Ilya <ilya-katkov-95@mail.ru>
2023-08-23 21:49:09 +0300

    solving

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
+from src import item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_calculate_total_price():
+    check_class = item.Item("Смартфон", 10000, 20)
+    assert check_class.calculate_total_price() == 200000
+
+def test_apply_discount():
+    check_class = item.Item("Смартфон", 10000, 20)
+    assert check_class.apply_discount() == None
